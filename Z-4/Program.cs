87cb79939bdc2aea7b0b5c1c35bde3da811a0813
Anatolii <zavorinanatolii@mail.ru>
2022-08-10 23:03:35 +0800

    Задание4

---
+++ Z-4/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Введите первое число : ");
+int N1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число : ");
+int N2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число : ");
+int N3 = int.Parse(Console.ReadLine());
+
+
+int result = N1 % N2;
+int result1 = N1 % N3;
+
+if (result  == 0)
+    {
+        if (result1 == 0)
+        {
+            Console.Write($"{N1} кратно {N2} и {N3}");
+        }
+        else
+    
+            
+        {
+            Console.Write($"{N1} не кратно одновременно {N2} и {N3}"); 
+        }
+        
+    }
+else
+{Console.Write($"{N1} не кратно одновременно {N2} и {N3}"); 
+} 
