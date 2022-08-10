51d76c9252ea7aba5b9c4c37e414351ff3e38f50
Anatolii <zavorinanatolii@mail.ru>
2022-08-10 23:49:20 +0800

    Задание5

--- Z-4.1/Program.cs
+++ Z-4.1/Program.cs
@@ -9,7 +9,7 @@ int N3 = int.Parse(Console.ReadLine());
 int result = N1 % N2;
 int result1 = N1 % N3;
 
-if (result  == 0 && result1 == 0)
+if (result  == 0 && result1 == 0) /* && логическая "и" - делает проверку result1 если result == 0  - истина (true) */
     {
          Console.Write($"{N1} кратно {N2} и {N3}");
     }
