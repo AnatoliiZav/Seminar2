78756a52fbcd39770e60f74523f5b088e1888dcc
Anatolii <zavorinanatolii@mail.ru>
2022-08-10 23:46:12 +0800

    Задание5

---
+++ Z-5/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое число : ");
+int N1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число : ");
+int N2 = int.Parse(Console.ReadLine());
+
+
+
+if (N1 * N1  == N2 || N2 * N2 == N1) /* "||" логическая "или" - делает проверку  на истину */
+    {
+         Console.Write("да");
+    }
+        
+else
+{Console.Write("нет"); 
+} 
+
