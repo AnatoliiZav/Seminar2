b3bb40cd7c129529cfe6c2feb43a8af8463f7caf
Anatolii <zavorinanatolii@mail.ru>
2022-08-10 22:05:28 +0800

    Задание3

---
+++ Z-3/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое число : ");
+int N1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число : ");
+int N2 = int.Parse(Console.ReadLine());
+
+int result = N1 % N2;
+/* можно убрать int result = N1 % N2; , тогда if (N1 % N2 == 0) */
+
+if (result == 0)
+{
+    Console.Write($"{N1} кратно {N2}");
+}
+else
+{
+    Console.Write($"{N1} не кратно {N2}, остаток {result}"); /* тогда здесь вместо result пишем N1 % N2 */
+}
